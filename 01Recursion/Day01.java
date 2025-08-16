53cc072075183d100e711a8e92e62508c71217fe
rohitrajpurohit17 <rajpurohit9673@gmail.com>
2025-08-16 16:07:16 +0530

    Added Recursion Folder

---
+++ 01Recursion/Day01.java
@@ -0,0 +1,23 @@
+class RecursionDemo{
+    int count = 1;
+    public void show(){
+        System.out.println(count);
+
+        // if this conditon is not there then it will run infintly
+        if(count == 5){
+            return;
+        }
+        count++;
+
+        // Here Function is calling itself in Function
+        show();
+    }   
+}
+
+public class Day01 {
+
+    public static void main(String[] args) {
+        RecursionDemo r = new  RecursionDemo();
+        r.show();
+    }
+}
